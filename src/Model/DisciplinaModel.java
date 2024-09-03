ae59802d81b14675ae829784ba8bbc30d314d939
George Oliveira <georgeazevedo16@gmail.com>
2024-09-02 23:45:06 -0300

    Adicionado atributos as classes
    DisciplinaModel
    InscricaoModel

--- src/Model/DisciplinaModel.java
+++ src/Model/DisciplinaModel.java
@@ -1,5 +1,51 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class DisciplinaModel {
+	
+	private String nomeDaDisciplina;
+	private String qtdVagasVoluntario;
+	private String qtdVagasRemunerada;
+	private ArrayList<InscricaoModel> inscricoes;
+	
+	public DisciplinaModel(String nomeDaDisciplina, String qtdVagasVoluntario, String qtdVagasRemunerada) {
+		this.nomeDaDisciplina = nomeDaDisciplina;
+		this.qtdVagasVoluntario = qtdVagasVoluntario;
+		this.qtdVagasRemunerada = qtdVagasRemunerada;
+		this.inscricoes = new ArrayList<>();
+	}
+
+	public String getNomeDaDisciplina() {
+		return nomeDaDisciplina;
+	}
+
+	public void setNomeDaDisciplina(String nomeDaDisciplina) {
+		this.nomeDaDisciplina = nomeDaDisciplina;
+	}
+
+	public String getQtdVagasVoluntario() {
+		return qtdVagasVoluntario;
+	}
+
+	public void setQtdVagasVoluntario(String qtdVagasVoluntario) {
+		this.qtdVagasVoluntario = qtdVagasVoluntario;
+	}
+
+	public String getQtdVagasRemunerada() {
+		return qtdVagasRemunerada;
+	}
+
+	public void setQtdVagasRemunerada(String qtdVagasRemunerada) {
+		this.qtdVagasRemunerada = qtdVagasRemunerada;
+	}
+
+	public ArrayList<InscricaoModel> getInscricoes() {
+		return inscricoes;
+	}
+
+	public void setInscricoes(ArrayList<InscricaoModel> inscricoes) {
+		this.inscricoes = inscricoes;
+	}
 
 }
--- src/Model/InscricaoModel.java
+++ src/Model/InscricaoModel.java
@@ -2,4 +2,70 @@ package Model;
 
 public class InscricaoModel {
 
+	private AlunoModel aluno;
+	private DisciplinaModel disciplina;
+	private float notaCRE;
+	private float notaDisciplina;
+	private float notaFinal;
+	private String resultado;
+
+	public InscricaoModel(AlunoModel aluno, DisciplinaModel disciplina, float notaCRE, float notaDisciplina,
+			String resultado, float notaFinal) {
+		this.aluno = aluno;
+		this.disciplina = disciplina;
+		this.notaCRE = notaCRE;
+		this.notaDisciplina = notaDisciplina;
+		this.notaFinal = notaFinal;
+		this.resultado = resultado;
+
+	}
+
+	public AlunoModel getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(AlunoModel aluno) {
+		this.aluno = aluno;
+	}
+
+	public DisciplinaModel getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(DisciplinaModel disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	public float getNotaCRE() {
+		return notaCRE;
+	}
+
+	public void setNotaCRE(float notaCRE) {
+		this.notaCRE = notaCRE;
+	}
+
+	public float getNotaDisciplina() {
+		return notaDisciplina;
+	}
+
+	public void setNotaDisciplina(float notaDisciplina) {
+		this.notaDisciplina = notaDisciplina;
+	}
+
+	public float getNotaFinal() {
+		return notaFinal;
+	}
+
+	public void setNotaFinal(float notaFinal) {
+		this.notaFinal = notaFinal;
+	}
+
+	public String getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+
 }
